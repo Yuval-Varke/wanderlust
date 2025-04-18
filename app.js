6339a705ed9683a4c76bd9991b13f98129c1fe82
Yuval-Varke <varke.vishu01@gmail.com>
2025-04-18 12:22:13 +0530

    .

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+// Add these middlewares
+app.use(session({
+    secret: 'yourSecretKey',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
@@ -43,50 +52,50 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
-  touchAfter: 24 * 3600,
-});
-
-store.on("error", () => {
-  console.log("ERROR in MONGO STORE", err);
-});
-
-const sessionOptions = {
-  store,
-  secret: process.env.SECRET,
+app.use(session({
+  secret: process.env.SECRET || "defaultsecret",
   resave: false,
-  saveUninitialized: true,
-  cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-  },
-};
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.ATLASDB_URL,
+    crypto: { secret: process.env.SECRET },
+  }),
+}));
+
+app.use((req, res, next) => {
+  res.locals.currUser = req.user; // Assuming you're using Passport.js
+  next();
+});
+// Make flash messages and current user available in all templates
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user; // If you're using Passport
+  next();
+});
+
+
+app.use((req, res, next) => {
+  console.log("Current User:", req.user); // Debugging line
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user || null;
+  next();
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Hi, I am root");
 // });
 
-app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next) => {
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
-  next();
-});
+
 
 // app.get("/demouser", async (req, res) => {
 //   let fakeUser = new User({
